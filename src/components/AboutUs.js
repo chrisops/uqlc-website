e2ca8105a2464084ef7534d3f835a9f170060271
chris <homebase.op@gmail.com>
2021-07-16 12:51:51 +1000

    added editable forms and new post component

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,9 +3,12 @@ import {context} from '../appReducer'
 
 export default function AboutUs() {
 
+  
+  const {about} = React.useContext(context)
+
+  /*const [text,setText] = React.useState(about)
   const [edit,setEdit] = React.useState(false)
-  const {about, dispatch} = React.useContext(context)
-  const [text,setText] = React.useState(about)
+  const {dispatch} = React.useContext(context)
 
   function editAbout(event){
     if (edit){
@@ -15,17 +18,24 @@ export default function AboutUs() {
       })
     }
       setEdit(!edit)
-  }
+  }*/                         // Disabled feature - editable about us 
 
   return (
     <>
       <h2>About Us</h2>
-        {edit ? 
+        {/* edit ? 
             <textarea onChange={(e)=>setText(e.target.value)}style={{margin: '20px 25%'}} rows="10" cols="70" placeholder='Write a new post...' id='body'>{about}</textarea>
           : 
-            <p style={{margin: '20px 25%'}}>{about}</p>}
+            <p style={{margin: '20px 25%'}}>{about}</p>
+            
+            =Disabled feature= - editable about us page
+
+        */}
+
+          <p style={{margin: '20px 25%'}}>{about}</p> {/* About Us */}
+          
           <br/>
-          <button onClick={editAbout}>{!edit ? 'Edit' : 'Save' }</button>
+          {/*<button onClick={editAbout}>{!edit ? 'Edit' : 'Save' }</button>  =Disabled feature= - editable about us page */ }
           <br/><br/>
       
       <img alt='placeholder' src="https://via.placeholder.com/250" />
