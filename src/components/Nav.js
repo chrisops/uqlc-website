3dc088e173886f8a5963b5f7e945a9a2eb491bba
chris <homebase.op@gmail.com>
2021-07-19 10:24:57 +1000

    some changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Nav() {
+export default function Nav({openModal}) {
   return (
       <nav style={{display:'flex',justifyContent: 'space-between',margin:'0px 25%'}}>
         <Link to='/'>Home</Link>
         <Link to='/events'>Events</Link>
         <Link to='/contact'>Contact</Link>
-        <Link to='/login'>Log in</Link>
-        <Link to='/signup'>Sign up</Link>
+        <button onClick={openModal({show: true, type: 'Log in'})}>Log in</button>
+        <button onClick={openModal({show: true, type: 'Sign up'})}>Sign up</button>
       </nav>
   );
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,13 +1,50 @@
 import React from 'react';
 
 export default function Signup() {
+
+  const [creds,setCreds] = React.useState({email: "",password: "", confirm: ""})
+
+  function formStateUpdate(event) {
+    setCreds({...creds, [event.target.id]: event.target.value})
+  }
+
+  async function formSubmit(event) {
+
+    event.preventDefault()
+    /*
+    let response = await fetch(`${env.API_URL}/signup`,{
+      method: 'POST',
+      body: JSON.stringify(creds),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    
+    let data = await response.json()
+
+    if (response.state === 200){
+      // successful login
+    }
+    else{
+      // failed login
+      data.error // contains error
+    }
+
+    
+    TO DO: 
+     - add dotenv for dev environment
+     - add logic for handling successful login
+     - store token response in state context
+    */
+  }
+
   return (
     <>
       <h2>Sign up</h2>
-      <form>
-        <input type='text' placeholder='Email'></input><br/><br/>
-        <input type='password' placeholder='Password'></input><br/><br/>
-        <input type='password' placeholder='Confirm Password'></input><br/><br/>
+      <form onSubmit={formSubmit} onChange={formStateUpdate}>
+        <input id='email' type='email' placeholder='Email'></input><br/><br/>
+        <input id='password' type='password' placeholder='Password'></input><br/><br/>
+        <input id='confirm' type='password' placeholder='Confirm Password'></input><br/><br/>
         <input type='submit' value='Register' />
       </form>
     </>
