bb42c8412324acbbd3bb935c5f7e31fa8ec9b44a
Chris Cotter <chris@Chriss-MacBook-Air.local>
2021-07-27 16:49:48 +1000

    stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+> ## R4	**Demonstrate your ability to work in a team**:
+
+> ## R5	Produce a working application that meets client and user needs
--- src/Stubs.js
+++ src/Stubs.js
@@ -19,7 +19,7 @@ const stubs = {
           image: 'https://via.placeholder.com/150'
         },
       ],
-    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique, leo sed tincidunt elementum, felis nisl tempor nisl, id lacinia mauris ex ut neque. Aenean finibus enim egestas, luctus ex sit amet, placerat arcu. Morbi nec imperdiet enim. Nunc maximus tincidunt interdum. Nulla nisi lacus, faucibus at augue sed, venenatis finibus nibh. Cras nec hendrerit arcu, non accumsan nulla. Sed nisi turpis, sagittis non pellentesque sit amet, hendrerit id purus. Quisque ultricies sapien et venenatis gravida. Vestibulum lacus tellus, lacinia a nisi id, dignissim porta magna. Donec mattis pharetra sapien id pharetra. Maecenas dictum gravida tortor a dignissim.'
+    about: 'UQ Lacrosse Club is dedicated to growing the sport of lacrosse within Brisbane. Although we are a UQ Sport-affiliated club, we don\'t require our members to be UQ students or staff. Contact us to learn more!'
 }
 
 export default stubs 
